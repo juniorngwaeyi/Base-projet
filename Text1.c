4cebb54e2f8326ab1b59d3ca2f636b2a7e321146
juniorngwaeyi257@gmail.com <juniorngwaeyi25@gmail.com>
2026-04-23 13:47:34 +0100

    de but de projet

---
+++ Text1.c
@@ -0,0 +1,15 @@
+#include"raylib.h"
+#define CREEN_HEIGHT 80
+#define CREEN_WIDTH 150
+#define FPS 60
+
+int main(void){
+
+    InitWindow(CREEN_WIDTH, CREEN_HEIGHT, "Text");
+
+    while(!windowshouldclose()){
+
+    }
+
+    CloseWindow();
+}
